289d459daeecfd4287b6b8bbc999d096c2218092
Douglas Camargo <corvo@Douglass-MacBook-Pro.local>
2021-04-16 17:55:10 -0300

    last winner

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 
 // react-native-gesture-handler
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+// react-native-linear-gradient
+import com.BV.LinearGradient.LinearGradientPackage;
 // react-native-safe-area-context
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 // react-native-screens
@@ -65,6 +67,7 @@ public class PackageList {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
       new RNGestureHandlerPackage(),
+      new LinearGradientPackage(),
       new SafeAreaContextPackage(),
       new RNScreensPackage(),
       new SvgPackage()
--- src/pages/main/Home.js
+++ src/pages/main/Home.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, TextInput, StatusBar, Dimensions, Keyboard } from 'react-native'
+import { StyleSheet, Text, View, TextInput, StatusBar, Dimensions, Keyboard, Platform, Image } from 'react-native'
 import { RFPercentage } from 'react-native-responsive-fontsize'
 import { useNavigation } from '@react-navigation/native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import LinearGradient from 'react-native-linear-gradient';
 import Swiper from 'react-native-swiper'
 
 import Logo from '../../assets/icons/Logo.svg';
+import Mocha from '../../assets/png/Mocha.png';
 
 
 export default function aaa() {
     const navigation = useNavigation()
     const [currentIndex, setCurrentIndex] = useState(0)
+    const [currentTwoIndex, setCurrentTwoIndex] = useState(0)
 
     function handleNextSwipe(index) {
         setCurrentIndex(index)
+    }
+    function handleNextSwipeTwo(index) {
+        setCurrentTwoIndex(index)
     }
 
     return (
@@ -30,26 +36,29 @@ export default function aaa() {
                 activeDotStyle={{}}>
                 <View style={styles.swiperContainer}>
                     <View style={styles.shoeContainer}>
-
+                        <Image style={styles.imageShoe} source={Mocha} />
                     </View>
+
                     <View style={styles.infoContainer}>
                         <View style={{ flex: 1.5, paddingRight: 10 }}>
-                            <Text style={styles.nameShoe}>Air Jordan 1 Unversity Blue</Text>
+                            <Text style={styles.nameShoe}>Air Jordan 1 Dark Mocha</Text>
                         </View>
-                        <View style={styles.containerPrize}
-                            size="small"
-                            rounded
-                            onPress={() => console.log("Works!")}
-                            activeOpacity={0.7}
-                            containerStyle={{ flex: 2, marginLeft: 20, marginTop: 115 }}>
-                            <Text style={styles.prizeDraw}>SORTEIO</Text>
-                            <Text style={styles.drawNumber}>#004</Text>
-                        </View>
-                        <View style={{ flex: 1.5, paddingRight: 10 }}>
+                        <View style={{ flex: 1, paddingRight: 10 }}>
                             <Text style={styles.valueShoe}>R$25,00</Text>
                             <Text style={styles.valueText}>Preço por número</Text>
                         </View>
                     </View>
+
+                    <LinearGradient style={styles.containerPrize}
+                        colors={['#0D0D0D', '#707070']}
+                        size="small"
+                        rounded
+                        onPress={() => console.log("Works!")}
+                        activeOpacity={0.7}
+                        containerStyle={{ flex: 1, marginLeft: 20, marginTop: 115 }}>
+                        <Text style={styles.prizeDraw}>SORTEIO</Text>
+                        <Text style={styles.drawNumber}>#004</Text>
+                    </LinearGradient>
                 </View>
             </Swiper>
 
@@ -58,6 +67,23 @@ export default function aaa() {
             </View>
 
             <Text style={styles.textTitle2}>ULTIMOS GANHADORES</Text>
+
+            <Swiper
+                loop={true}
+                autoplay={true}
+                showsButtons={false}
+                showsPagination={false}
+                onIndexChanged={handleNextSwipeTwo}
+                activeDotStyle={{}}>
+                <View style={styles.swiperContainerTwo}>
+                    <View style={styles.infoWinner}>
+                        <Text style={styles.nameShoeTwo}>Air Jordan 1 Dark Mocha</Text>
+                        <Text style={styles.nameWinner}>IVAN OLIVEIRA</Text>
+                        <Text style={styles.infoData}>SORTEIO FINALIZADO EM:</Text>
+                        <Text style={styles.dataPrize}>03/04/21</Text>
+                    </View>
+                </View>
+            </Swiper>
 
             <StatusBar barStyle="light-content" />
         </View>
@@ -76,22 +102,25 @@ const styles = StyleSheet.create({
     logo: {
         alignSelf: "baseline",
         marginLeft: DEVICE_WIDTH * .1,
-        // backgroundColor: 'yellow',
         marginTop: Platform.OS === 'ios' ? DEVICE_HEIGHT * .035 : DEVICE_HEIGHT * -.01,
     },
+
+
     textTitle: {
         color: '#fff',
         textAlign: 'right',
         alignSelf: "baseline",
         fontSize: RFPercentage(2.5),
-        marginTop: DEVICE_WIDTH * .0,
         marginLeft: DEVICE_WIDTH * .1,
         marginBottom: DEVICE_WIDTH * .03,
         fontFamily: 'Montserrat-Black',
     },
+
+
     swiperContainer: {
         alignSelf: "center",
     },
+
     shoeContainer: {
         width: DEVICE_WIDTH * .8,
         height: DEVICE_HEIGHT * .179,
@@ -99,11 +128,17 @@ const styles = StyleSheet.create({
         borderTopRightRadius: 20,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#fff',
+        backgroundColor: '#f4f4f4',
+    },
+    imageShoe: {
+        resizeMode: 'contain',
+        width: DEVICE_WIDTH * .45,
+        height: DEVICE_HEIGHT * .45,
     },
     infoContainer: {
         width: DEVICE_WIDTH * .8,
-        height: DEVICE_HEIGHT * .14,
+        // height: DEVICE_HEIGHT * .14,
+        height: Platform.OS === 'ios' ? DEVICE_HEIGHT * .13 : DEVICE_HEIGHT * .1,
         borderBottomLeftRadius: 20,
         borderBottomRightRadius: 20,
         alignItems: 'center',
@@ -117,23 +152,31 @@ const styles = StyleSheet.create({
         fontFamily: 'Montserrat-Black',
     },
     containerPrize: {
-        textAlign: 'center',
         backgroundColor: '#000',
-        padding: 10,
-        borderRadius: 100,
-
+        justifyContent: 'center',
+        // alignSelf: 'center',
+        borderRadius: 50,
+        position: 'absolute',
+        // marginTop: Platform.OS === 'ios' ? DEVICE_HEIGHT * .137 : DEVICE_HEIGHT * .137,
+        marginTop: Platform.OS === 'ios' ? DEVICE_HEIGHT * .01 : DEVICE_HEIGHT * .01,
+        marginLeft: Platform.OS === 'ios' ? DEVICE_HEIGHT * .01 : DEVICE_HEIGHT * .01,
+        width: Platform.OS === 'ios' ? DEVICE_HEIGHT * .085 : DEVICE_HEIGHT * .082,
+        height: Platform.OS === 'ios' ? DEVICE_HEIGHT * .085 : DEVICE_HEIGHT * .082,
     },
     prizeDraw: {
         textAlign: 'center',
         fontSize: RFPercentage(1),
-        fontFamily: 'Montserrat-light',
-        color: '#fff'
+        fontFamily: 'Montserrat-Medium',
+        color: '#fff',
+        alignSelf: 'center',
+        letterSpacing: 0.5,
     },
     drawNumber: {
         textAlign: 'center',
-        fontSize: RFPercentage(1.3),
+        fontSize: RFPercentage(2.2),
         fontFamily: 'Montserrat-Black',
-        color: '#fff'
+        color: '#fff',
+        alignSelf: 'center',
     },
     valueShoe: {
         textAlign: 'right',
@@ -147,12 +190,13 @@ const styles = StyleSheet.create({
         paddingRight: 10,
         fontFamily: 'Montserrat-Light',
     },
+
+
     swiper: {
-        marginBottom: Platform.OS === 'ios' ? DEVICE_HEIGHT * .00 : DEVICE_HEIGHT * .00,
         justifyContent: 'center',
         flexDirection: 'row',
-        height: 23,
         alignItems: 'center',
+        marginTop: 10,
     },
     dot: {
         marginHorizontal: 4,
@@ -162,6 +206,7 @@ const styles = StyleSheet.create({
         borderWidth: 1.4,
         borderRadius: 8,
     },
+
     textTitle2: {
         color: '#fff',
         textAlign: 'left',
@@ -169,6 +214,42 @@ const styles = StyleSheet.create({
         fontSize: RFPercentage(2.5),
         fontFamily: 'Montserrat-Black',
         marginLeft: DEVICE_WIDTH * .1,
-        marginBottom: Platform.OS === 'ios' ? DEVICE_HEIGHT * .31 : DEVICE_HEIGHT * .24,
+        marginTop: Platform.OS === 'ios' ? DEVICE_HEIGHT * .013 : DEVICE_HEIGHT * .013,
+        // marginBottom: Platform.OS === 'ios' ? DEVICE_HEIGHT * .3 : DEVICE_HEIGHT * .23,
+    },
+
+
+    swiperContainerTwo: {
+        alignSelf: "center",
+        width: DEVICE_WIDTH * .8,
+        marginTop: Platform.OS === 'ios' ? DEVICE_HEIGHT * .015 : DEVICE_HEIGHT * .015,
+    },
+    infoWinner: {
+        borderRadius: 20,
+        padding: 10,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#4FFC91',
+    },
+    nameShoeTwo: {
+        fontSize: RFPercentage(2.3),
+        fontFamily: 'Montserrat-Black',
+        textAlign: 'center',
+        marginBottom: 4,
+    },
+    nameWinner: {
+        fontSize: RFPercentage(2),
+        fontFamily: 'Montserrat-Regular',
+        textAlign: 'center',
+    },
+    infoData: {
+        marginTop: 4,
+        fontSize: RFPercentage(1.8),
+        fontFamily: 'Montserrat-Light',
+    },
+    dataPrize: {
+        fontSize: RFPercentage(2),
+        fontFamily: 'Montserrat-Bold',
     },
 })
